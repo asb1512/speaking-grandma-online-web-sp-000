8dc28272d58b67fe8372265feea26375cc5b0b78
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-03 03:29:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  
+  if phrase == "I LOVE YOU GRANDMA!"
+    
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
